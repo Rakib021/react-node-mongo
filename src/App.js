82d0e331edd216494fba001d8e3a5cdbe6cbb6e5
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-20 21:24:38 +0600

    insert,delete,update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import AddUser from './components/AddUser/AddUser';
 import Home from './components/Home/Home';
+import Update from './components/update/Update';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path ="/home" element={<Home></Home>}></Route>
+        <Route path ="/" element={<Home></Home>}></Route>
         <Route path="/add/user" element={<AddUser></AddUser>}></Route>
+        <Route path ="/update/:id" element={<Update></Update>}></Route>
 
       </Routes>
     </div>
--- src/components/update/Update.js
+++ src/components/update/Update.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const AddUser = () => {
-    const handleSubmit = event =>{
+
+const Update = () => {
+    const {id} = useParams();
+    const [user,setUser] = useState({});
+    useEffect(()=>{
+        const url = `http://localhost:4000/user/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data =>setUser(data))
+    },[])
+
+    const handleUpdateSubmit = event =>{
         const name = event.target.name.value;
         const email = event.target.email.value;
         const data ={
@@ -16,8 +27,9 @@ const AddUser = () => {
         // const user =(name,email);
 
         //send data to the server
-fetch('http://localhost:4000/user',{
-    method :'POST',
+        const url = `http://localhost:4000/user/${id}`;
+fetch(url,{
+    method :'PUT',
     body :JSON.stringify(data),
     headers : {
         'content-type' : 'application/json'
@@ -35,17 +47,18 @@ fetch('http://localhost:4000/user',{
     }
     return (
         <div>
-            <h2>please add a user</h2>
-            <form onSubmit={handleSubmit}>
+            <h2>updating : {user.name}</h2>
+            <form onSubmit={handleUpdateSubmit}>
                 <input type="text" name="name" placeholder ="Name" id="" required/>
                 <br/>
                 <input type="email" name="email" placeholder="email" id="" required/>
                 <br/>
-                <input type="submit" value="Add user"/>
+                <input type="submit" value="Update user"/>
             </form>
             <ToastContainer/>
+            
         </div>
     );
 };
 
-export default AddUser;
+export default Update;
